4d92d720699b6c49721b26be69e7689e132df135
Prajwol Gyawali <prajwol@ibriz.ai>
2023-03-31 15:43:45 +0545

    Utilize records instead of mapping for tips

--- execute.sh
+++ execute.sh
@@ -1,7 +1,7 @@
 PRIVATEKEY="APrivateKey1zkpGWbPXNScC62Ww5jHxFTWa6st1wosnuKd693QVfHKyX7K"
 snarkos developer execute \
-"posts.aleo" "post" "2793123896416649578508430956173875066425468388805468715479907750778834469731416946970field" \
+"postsVI.aleo" "post" "1field" "1field" "1field" \
 --private-key "${PRIVATEKEY}" \
 --query "https://vm.aleo.org/api" \
-# --broadcast "https://vm.aleo.org/api/testnet3/transaction/broadcast"
--- display
+--broadcast "https://vm.aleo.org/api/testnet3/transaction/broadcast"
+# --display 
